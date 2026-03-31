d36b3258be122d5b5e3d41d0d458d95767a4c3b4
miiyao7 <maihong@hotmail.ca>
2026-03-31 15:51:33 -0400

    modified logo color

--- frontend/src/features/auth/components/AuthLayou.tsx
+++ frontend/src/features/auth/components/AuthLayou.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import authLogo from "../../../assets/logo.png";
+import authLogo from "../../../assets/logoo.png";
 
 interface Props {
     title: string;
